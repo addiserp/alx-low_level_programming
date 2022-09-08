a2a337f9fbbdf6f0916714c77fc4dc52dbd454d5
Mikias Gedlu <addiserp@gmail.com>
2022-09-08 15:04:34 -0700

    4-puts.c commited

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+
+ * main - Entry print 
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+  
+{
+  
+  printf("Programming is like building a multilingual puzzle\n");
+  
+  return (0);
+  
+}
+
