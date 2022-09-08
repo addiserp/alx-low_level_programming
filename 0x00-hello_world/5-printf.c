908e0e106aab79d04861fa17c3420457339fd7cd
Mikias Gedlu <addiserp@gmail.com>
2022-09-08 15:18:38 -0700

    5-printf.c commited

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+
+ * main - print the word in the put function
+
+ *
+
+ * Description: using the main function
+
+ * this program puts out "Programming is like building a multilingual puzzle
+
+ * Return: 0
+
+ */
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
+}
