c649921850020371f79b8a0a9d13d662c3111a39
mikias gedlu <mikiasgd@gmail.com>
2022-09-09 23:12:59 +0300

    100-print_comb3.c commited bonus

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,7 +4,7 @@
  * main - entry point
  *
  * Description: this is the main function program
- * this uses putchar and prints only 0-9 with comma and space
+ * this uses putchar and prints  different combinations of two digits.
  * Return: is always 0
  */
 int main(void)
@@ -16,11 +16,12 @@ for (d = 11; d <= 19; d++)
 {
 putchar(c % 10 + '0');
 putchar(d % 10 + '0');
-}
+
 if (c <= 18)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
