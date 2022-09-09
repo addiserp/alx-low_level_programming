4ba43c0efd53b447979cf8a5fd95586c511bea08
Mikias Gedlu <addiserp@gmail.com>
2022-09-09 06:44:19 -0700

    5-printf.c commited

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
 /**
-
  * main - print the word in the put function
-
  *
-
  * Description: using the main function
-
  * this program puts out "Programming is like building a multilingual puzzle
-
  * Return: 0
-
  */
-
 int main(void)
-  
 {
-  
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  
-  return (0);
-  
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
