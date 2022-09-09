189512bebe75e04dd126ca6c7e27493363b4b06f
Mikias Gedlu <addiserp@gmail.com>
2022-09-09 06:35:57 -0700

    4-puts.c commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 
 /**
-
  * main - enrty point
-
  *
-
  * Description: this is the main fist c program
-
  * this program puts "Programming is like building a multilingual puzzle
-
  * Return: is alwasys 0
-
  */
 
 int main(void)
-  
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0);
 }
 
