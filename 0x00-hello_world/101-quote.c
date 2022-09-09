3c5abf4f900cf3a1c664da8f29c7b806a5e8ab2b
Mikias Gedlu <addiserp@gmail.com>
2022-09-09 06:51:25 -0700

    101-quote.c commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
-
-
-
 /**
-
  * main - print the string in the put function
-
  *
-
  * Description: using the main function
-
  * this program prints and that piece of art is useful" - Dora Korpar, 2015-10-19,
-
  * Return: 1
-
  */
-
 int main(void)
-  
 {
-  
-  printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-  
-  return (1);
-  
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
