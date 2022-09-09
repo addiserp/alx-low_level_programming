7f3ab61a2885faa3f15a223977d1150a8dca8775
mikias gedlu <mikiasgd@gmail.com>
2022-09-10 00:06:24 +0300

    102-print_comb5.c commited

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,7 +4,7 @@
  * main - entry point
  *
  * Description: this is the main function program
- * this uses putchar and prints  different combinations of four with pairing digits.
+ * different combinations of four with pairing digits.
  * Return: is always 0
  */
 int main(void)
@@ -12,14 +12,14 @@ int main(void)
 int c, d, e, f;
 for (c = 10; c <= 19; c++)
 {
-d = c + 1;
-for (; d <= 19; d++)
+/*d = c + 1;*/
+for (d = 10; d <= 19; d++)
 {
-e = d + 1;
-for (; e <= 19; e++)
+/*e = d + 1;*/
+for (e = 10; e <= 19; e++)
 {
-f = e + 1;
-for (; f <= 19; f++)
+/*f = e + 1;*/
+for (f = 10; f <= 19; f++)
 {
 putchar(c % 10 + '0');
 putchar(d % 10 + '0');
