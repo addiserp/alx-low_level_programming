a700d41e0e5b3116d067efa2fe0bea3b4395cae9
Mikias Gedlu <addiserp@gmail.com>
2022-09-09 09:49:14 -0700

    2-print_alphabet.c commited

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: this is the main function program
+ * this uses putchar and prints a -z
+ * Return: is always 0
+ */
+int main(void)
+{
+  for(int x = 'a'; x <= 'z'; x++)
+    {
+      putchar(x);
+      putchar('\n');
+    }
+  return(0);
+}
