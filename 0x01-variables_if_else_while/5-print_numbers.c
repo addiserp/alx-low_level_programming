97e3e66e8bd3433352087eab1c2783c9a86f1ecb
mikias gedlu <mikiasgd@gmail.com>
2022-09-09 21:52:54 +0300

    5-print_numbers.c commited

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: this is the main function program
+ * print from 0 -9
+ * Return: is always 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+printf("%d",x);
+return (0);
+}
