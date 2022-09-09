0d80a26c1b25b177bb57f312fcf6551c3dbba405
mikias gedlu <mikiasgd@gmail.com>
2022-09-09 23:48:10 +0300

    101-print_comb4.c commited bouns

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: this is the main function program
+ * this uses putchar and prints  different combinations of three digits.
+ * Return: is always 0
+ */
+int main(void)
+{
+int c, d;
+for (c = 10; c <= 19; c++)
+{
+d = c + 1;
+for (; d <= 19; d++)
+{
+e = d + 1;
+for (; e <= 19; e++)
+{
+putchar(c % 10 + '0');
+putchar(d % 10 + '0');
+putchar(e % 10 + '0');
+if (c <= 18 && d <= 19 && !(c == 18 && d == 19))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
