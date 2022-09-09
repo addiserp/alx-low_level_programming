4f6069f019d37a2de6ddd82909ed4eded3c51fd4
mikias gedlu <mikiasgd@gmail.com>
2022-09-09 21:58:24 +0300

    6-print_numberz.c commited

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: this is the main function program
+ * print from 0 -9
+ * Return: is always 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
