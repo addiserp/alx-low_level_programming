1d6815c57ad0a594fc28421c02cbdc88a364ec3a
mikias gedlu <mikiasgd@gmail.com>
2022-09-13 18:56:42 +0300

    hot fix 7

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,6 +8,7 @@
  /* Description: print_alphabet print alphabet */
 void print_alphabet(void)
 {
+ /* Description: print_alphabet print alphabet */
 int x;
 for (x = 'a'; x <= 'z'; x++)
 _putchar(x);
