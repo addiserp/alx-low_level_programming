a56b5e4248675bcac719779682147565ae73702e
mikias gedlu <mikiasgd@gmail.com>
2022-09-13 16:16:57 +0300

    0-putchar.c commited

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: this is the main function program
+ * this program prints _putchar
+ * Return: is always 0
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
