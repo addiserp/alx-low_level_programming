280ca4d75432ed2ba95f0a2e665a79d6a241316a
mikias gedlu <mikiasgd@gmail.com>
2022-09-13 19:03:19 +0300

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+ /**
+ * main - entry point
+ * Description: print_alphabet print alphabet 10 times
+ * Description: this is the main function program
+ * Return: is void
+ */
+ /* Description: print_alphabet print alphabet */
+void print_alphabet(void)
+{
+ /* description: print_alphabet print alphabet */
+int x,y;
+for (y = 0; y < 10; y++)
+for (x = 'a'; x <= 'z'; x++)
+_putchar(x);
+_putchar('\n');
+}
