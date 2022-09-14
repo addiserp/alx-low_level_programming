fa87c2ae2ef15e84ab81dca9943f7177cd2c1172
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 16:41:23 +0300

    hot fix bounes 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,38 @@
 #include "main.h"
 /**
-*print_times_table : - prints the  times table with
-* print_times_table - prints the time table
-*@n: input parameter
-*Return: returns void
-*/
+ * print_times_table - prints time table
+ * n@: input parameter
+ * Return: is void
+ */
 void print_times_table(int n)
 {
 int d, m, x;
 if (n <= 15 && n >= 0)
-{
-for (d = 0; d <= n; d++)
-{
-_putchar('0');
-
-for (m = 1; m <= n; m++)
-{
-_putchar(',');
-_putchar(' ');
-x = d * m;
-if (x <= 99)
-_putchar(' ');
-
-if (x <= 9)
-_putchar(' ');
-if (x >= 100)
-{
-_putchar((x / 100) + '0');
-_putchar((x / 10) % 10 + '0');
-}
-else if (x <= 99 && x >= 10)
-{
-_putchar((x / 10) + '0');
-}
-_putchar((x % 10) + '0');
-}
-_putchar('\n');
-}
-}
+	{
+		for (d = 0; d <= n; d++)
+		{
+			_putchar('0');
+			for (m = 1; m <= n; m++)
+			{
+			_putchar(',');
+			_putchar(' ');
+			x = d * m;
+			if (x <= 99)
+				_putchar(' ');
+			if (x <= 9)
+			_putchar(' ');
+			if (x >= 100)
+			{
+				_putchar((x / 100) + '0');
+				_putchar((x / 10) % 10 + '0');
+			}
+			else if (x <= 99 && x >= 10)
+			{
+				_putchar((x / 10) + '0');
+			}
+			_putchar((x % 10) + '0');
+		}
+			_putchar('\n');
+		}
+	}
 }
