02f70e7820e2729ddb7cd3e8b7d655990ed3ea48
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 16:35:13 +0300

    print_times_table commited

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-*print_times_table: - prints the  times table with
+*print_times_table : - prints the  times table with
+* print_times_table - prints the time table
 *@n: input parameter
 *Return: returns void
 */
