4a751341606254b956c920e619b119fc99584aae
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 18:51:27 +0300

    hot fix 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,11 +5,12 @@
 */
 int main(void)
 {
-unsigned long x = 0, y= 1, z = 0, result = 0;
+unsigned long x = 0, y = 1, z = 0, result = 0;
 while (z <= 4000000)
 {
 z = y + x;
-x = y = z;
+x = y;
+y = z;
 if ((x % 2) == 0)
 result += x;
 }
