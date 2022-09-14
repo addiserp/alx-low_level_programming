8584bb9c5bd0a3f52ee0f0415b2dcfe72bffe180
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 16:50:11 +0300

    COMMITE 100-times-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,33 +6,31 @@
  */
 void print_times_table(int n)
 {
-int d, m, x;
-if (n <= 15 && n >= 0)
-{
-for (d = 0; d <= n; d++)
-{
-_putchar('0');
-for (m = 1; m <= n; m++)
-{
-_putchar(',');
-_putchar(' ');
-x = d * m;
-if (x <= 99)
-_putchar(' ');
-if (x <= 9)
-_putchar(' ');
-if (x >= 100)
-{
-_putchar((x / 100) + '0');
-_putchar((x / 10) % 10 + '0');
-}
-else if (x <= 99 && x >= 10)
-{
-_putchar((x / 10) + '0');
-}
-_putchar((x % 10) + '0');
-}
-_putchar('\n');
-}
-}
+	int d, m, x;
+	if (n <= 15 && n >= 0)
+	{
+		for (d = 0; d <= n; d++)
+		{
+			_putchar('0');
+			for (m = 1; m <= n; m++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				x = d * m;
+				if (x <= 99)
+				_putchar(' ');
+				if (x <= 9)
+				_putchar(' ');
+				if (x >= 100)
+				{
+				_putchar((x / 100) + '0');
+				_putchar((x / 10) % 10 + '0');
+				}
+				else if (x <= 99 && x >= 10)
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
