c0bf6d6ea06a0fe8005495c284c769a7c07ec86b
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 18:49:57 +0300

    104-fibonacci.c commited

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - prints result of even fibonacci
+ *Return: return 0
+*/
+int main(void)
+{
+unsigned long x = 0, y= 1, z = 0, result = 0;
+while (z <= 4000000)
+{
+z = y + x;
+x = y = z;
+if ((x % 2) == 0)
+result += x;
+}
+printf("%ld\n", result);
+return (0);
+}
