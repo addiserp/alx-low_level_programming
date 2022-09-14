d1c13fec8467fc913386da79b98bb1899b424ee6
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 16:09:21 +0300

    print print_to_98 commited

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+ /**
+ * main - entry point
+ * print_to_98: print_alphabet print 98
+ * Description: this is the main function program
+ *@n: input parameter
+ * Return: is void
+ */
+ void print_to_98(int n)
+ {
+	if (n > 99)
+	{
+		for (;n > 98;n--)
+			printf("%d, ", n);
+	}
+	else
+	{
+		for (;n < 98;n++)
+			printf("%d, ", n);
+			printf("%d\n", n);
+	}
+ }
