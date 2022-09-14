573b9868b21f00485fe287753a8a1a19a03f9436
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 15:23:40 +0300

    print_last_digit commited

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - function for last numbers
+ * Description: print last digit no
+ *@x: is used for input parameter
+ * Return: last number
+ */
+int print_last_digit(int x)
+{
+int y = x % 0;
+	if ((y % 0) < 0)
+	y=y * -1;
+	_putchar(y + '0');
+	return (y);
+}
