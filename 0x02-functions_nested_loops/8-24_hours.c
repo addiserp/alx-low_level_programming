4b75d4fcb62a8c9f724d55b7d4f21a80e3ea94c1
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 17:54:43 +0300

    hot fix for jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * jack_bauer - function for print day of minites
+ * jack_bauer - function day of minites
  * Description: print 24h in minite
  *@x: is used for input parameter
  * Return: void
  */
 void jack_bauer(void)
 {
+
 int min, hr;
 int mod_min, mod_hr;
 mod_hr = 0;
 mod_min = 0;
-for (hr = 0;hr < 24;hr++)
-	for (min =0 ;min < 60;min++)
+for (hr = 0; hr < 24; hr++)
+	for (min = 0 ; min < 60; min++)
 	{
 		mod_hr = hr % 10;
 		_putchar(hr / 10 + '0');
