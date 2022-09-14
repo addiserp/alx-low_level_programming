27c8b1f696948769d39a9baded5370c7098bef2e
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 17:25:23 +0300

    print_alphabet_x10 hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,9 @@
  * print_alphabet_x10 : this is the main function program
  * Return: is void
  */
- /* print_alphabet_x10: print_alphabet print alphabet */
 void print_alphabet_x10(void)
 {
- /* description: print_alphabet print alphabet */
+
 int x, y;
 for (y = 0; y < 10; y++)
 {
