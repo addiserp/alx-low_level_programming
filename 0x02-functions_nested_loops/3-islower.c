eccf20ececcccf98b45c11cb264d0a29fad219cd
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 14:11:16 +0300

    commited 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,11 @@
  * 3-islower - function started here
  * Description: check the alphabet is islower 
  * Return:0
- int _islower(int x)
- {
- 	 if (x >= a && x <= 'z')
-	 {
-	 	 return 1;
-	 }
-	 else
+ */
+int _islower(int x)
+{
+	if (x >= a && x <= 'z')
+		return 1;
+	else
 		return 0;
  }
