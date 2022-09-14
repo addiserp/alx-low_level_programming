1fc42390f6bb3ae8eafb665d05434b91fc0293b4
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 15:12:04 +0300

    ABS checker commited

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function for absolut numbers
+ * Description: absolute checker
+ *@x: is used for input parameter
+ * Return:0
+ */
+int _abs(int x)
+{
+	if (x == 0 || x > 0)
+		return (x);
+	else
+		return (c * -1);
+}
