bf21211320918845c959422abb21c539af2e88fc
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 14:07:55 +0300

    commited 3-islower - function st

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * 3-islower - function started here
+ * Description: check the alphabet is islower 
+ * Return:0
+ int _islower(int x)
+ {
+ 	 if (x >= a && x <= 'z')
+	 {
+	 	 return 1;
+	 }
+	 else
+		return 0;
+ }
