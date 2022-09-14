a943441fe97f262a83d1f4a17036cb68b6480b85
mikias gedlu <mikiasgd@gmail.com>
2022-09-14 17:04:51 +0300

    hot fix 4 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
  /**
  * print_alphabet - this is the main print_alphabet function
- * Description: print_alphabet print alphabet
+ * description - print_alphabet print alphabet
  * Return: is void
  */
 void print_alphabet(void)   
