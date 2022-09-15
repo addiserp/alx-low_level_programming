6cb45230beb5f0eb3c04c7dcedfb400b89b55876
mikias gedlu <mikiasgd@gmail.com>
2022-09-15 16:53:11 +0300

    hot fix for main.c 0 positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,21 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "main.h"
 /**
 * main - tests function that prints if integer is positive or negative
 * positive_or_negative - chech n is + or -
 * Return: 0
 */
-void positive_or_negative(int n)
-{
-
-if (n < 0)
-printf ("%d is negative\n", n);
-else if (n == 0)
-printf ("%d is zero\n", n);
-else if (n > 0)
-printf ("%d is positive\n", n);
-}
 int main(void)
 {
 	int i;
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
