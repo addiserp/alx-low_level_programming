95539c04f96edd72c3ec37d847695ea913d88653
mikias gedlu <addiserp@gmail.com>
2022-09-15 19:02:09 +0300

    commited form ubuntu

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,39 +1,16 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main  - check if char is upper alphabet
- * _putchar - writes the character c to stdout
- * _isupper - checks if it is upper case letter
- * @ch: The character to print
- * Return: always zero
+ * _isupper - check if char is upper alphabet
+ *@x: char parameter
+ * _isupper: checks if it is upper case letter
+ * Return: zero or one
  */
-/*description - _putchar a function */
-int _putchar(char ch)
-{
-/* description - this function prints char */
-return (write(1, &ch, 1));
-}
+
 int _isupper(int x)
 {
-/* description - this function compares chars */
 	if (x >= 'A' && x <= 'Z')
 	return (1);
 	else
-	return (0);
-}
-int main(void)
-{
-	char c;
-
-	c = 'A';
-	int result;
-
-	result = _isupper(c);
-
-	if (result == 1)
-	_putchar('1');
-	else
-	_putchar('0');
-
 	return (0);
 }
