a7b4dd69c5626d68cabca7881edd6a9e90b50dd9
mikias gedlu <mikiasgd@gmail.com>
2022-09-15 16:17:12 +0300

    0-main.c commited

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,20 +2,18 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
+* positive_or_negative - chech n is + or -
 * Return: 0
 */
 void positive_or_negative(int n)
 {
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
+
 if (n < 0)
 printf ("%d is negative\n", n);
 else if (n == 0)
 printf ("%d is zero\n", n);
 else if (n > 0)
 printf ("%d is positive\n", n);
-return (0);
 }
 int main(void)
 {
