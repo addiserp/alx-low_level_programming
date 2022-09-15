bfd8e7ebe7852578f0473f96d18454339b0da324
mikias gedlu <mikiasgd@gmail.com>
2022-09-15 16:57:20 +0300

    hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* main - tests function that prints if integer is positive or negative
-* positive_or_negative - chech n is + or -
-* Return: 0
+ * main - tests function that prints if integer is positive or negative
+ * positive_or_negative - chech n is + or -
+ * Return: 0
 */
 int main(void)
 {
 	int i;
 	i = 0;
 	positive_or_negative(i);
-    return (0);
+	return (0);
 }
--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -3,14 +3,13 @@
 #include <time.h>
 
 /**
-* main - main calls ositive_or_negative function
-* positive_or_negative - tests function that prints if integer is positive or negative
-* positive_or_negative - chech n is + or -
-* Return: void
-*/
+ * main - main calls ositive_or_negative function
+ * positive_or_negative - tests function that prints if integer is positive or negative
+ * positive_or_negative - chech n is + or -
+ * Return: void
+ */
 void positive_or_negative(int n)
 {
-
 if (n < 0)
 printf ("%d is negative\n", n);
 else if (n == 0)
@@ -23,5 +22,6 @@ int main(void)
 	int i;
 	i = 0;
 	positive_or_negative(i);
-    return (0);
+	return (0);
 }
+
