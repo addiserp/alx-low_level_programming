ec27119649e8cd50b65cf357412ccf7ba3229d0a
mikias gedlu <mikiasgd@gmail.com>
2022-09-16 23:32:07 +0300

    5-more_numbers.c commited

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *more_numbers - prints numbers to 14 10 times
+ *Return: returns void
+*/
+
+void more_numbers(void)
+{
+	int x1, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				putchar((y / 10) + '0');
+			}
+			putchar((y % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
