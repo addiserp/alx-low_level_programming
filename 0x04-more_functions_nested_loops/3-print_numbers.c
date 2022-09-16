02a35c253638a51fab2f9f82adbc77c3008fa3d3
mikias gedlu <addiserp@gmail.com>
2022-09-16 17:34:16 +0300

    3-print_numbers.c commited

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_numbers - main function to print from 0 - 9
+ *Description: takes no parameter prints from 0 - 9
+ *Return: returns nothing
+ */
+
+void print_numbers(void)
+{
+int x;
+for (x = 48; i < 58; i++)
+	_putchar(x);
+	_putchar('\n');
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _islower(int);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+void print_numbers(void);
 #endif
