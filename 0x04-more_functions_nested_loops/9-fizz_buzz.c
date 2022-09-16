3e717534f14f4e67830932bfab67d2301e4389db
mikias gedlu <mikiasgd@gmail.com>
2022-09-16 23:58:33 +0300

    9-fizz_buzz.c comited2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,10 +17,12 @@ int main(void)
 		else if ((x % 3) == 0)
 			printf("Fizz ");
 		else if ((x % 5) == 0)
+		{
 			if (x != 100)
 				printf("Buzz ");
 			else
 			printf("Buzz");
+		}
 		else
 			printf("%d ", x);
 	}
