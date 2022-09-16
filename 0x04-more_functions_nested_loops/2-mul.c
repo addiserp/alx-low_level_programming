ca00be95cef0007f8366bb6d4b3efa8e8bc3b6f5
mikias gedlu <addiserp@gmail.com>
2022-09-16 17:26:28 +0300

    2-mul.c commited

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - main function give the result of two int mulitiplication
+ *Description: takes 2 int values and muliply it
+ *@x: 1st value
+ *@y: 2nd value
+ *Return: returns the result
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,5 @@ int _putchar(char);
 int _islower(int);
 int _isupper(int);
 int _isdigit(int);
+int mul(int, int);
 #endif
