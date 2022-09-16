153d2be1fc0afe9ee439be077e05c3db83c26118
mikias gedlu <addiserp@gmail.com>
2022-09-16 17:52:14 +0300

    3-print_numbers.c commited

--- 0x04-more_functions_nested_loops/3-print_numbers2.c
+++ 0x04-more_functions_nested_loops/3-print_numbers2.c
@@ -1,16 +1,22 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 /**
  *print_numbers - main function to print from 0 - 9
+ *_putchar - writes the character c to stdout
+ *@c: the char to print
  *Description: takes no parameter prints from 0 - 9
  *Return: returns nothing
  */
-
+/*Description: function for _putchar */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void print_numbers(void)
 {
 int x;
 for (x = 48; x < 58; x++)
-	putchar(x);
-	putchar('\n');
+	_putchar(x);
+	_putchar('\n');
 }
 
