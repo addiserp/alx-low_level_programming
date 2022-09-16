5744ce678ce48a91c71bb776cf87d68d3319a6d6
mikias gedlu <mikiasgd@gmail.com>
2022-09-17 00:28:07 +0300

    bonus questions commited

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main- prints the largest prime factor of no
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	long n = 612852475143;
+	int i;
+
+	while (i++ < n / 2)
+	{
+	if (n % i == 0)
+	{
+		n /= 2;
+		continue;
+	}
+
+	for (i = 3; i < n / 2; i += 2)
+	{
+		if (n % i == 0)
+			n /= i;
+	}
+	}
+	printf("%ld\n", n);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - a function that Prints a number
+ * @n: a parameter
+ *Return: Reterns Void
+ */
+
+void print_number(int x)
+{
+	unsigned int n = x;
+
+	if (x < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n > 9)
+	{
+		print_number(n / 10);
+	}
+	putchar(n % 10 + '0');
+}
