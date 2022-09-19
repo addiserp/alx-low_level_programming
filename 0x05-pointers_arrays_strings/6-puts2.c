38709a524ba3d8fe54f7aa2073e924d6302bbbad
mikias gedlu <mikiasgd@gmail.com>
2022-09-20 02:20:38 +0300

    6--puts2 added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts2 - prints the every other  string
+ *@str: char parameter
+ * Return: returns nothing
+ */
+
+void _puts2(char *str)
+{
+	int s = 0;
+
+	while (*(str + s) != '\0')
+	{
+	_putchar(*(str + s));
+	s = s + 2;
+	}
+	_putchar('\n');
+}
