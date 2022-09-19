06af94fad0cdd00acb8201a532da0961c5585579
mikias gedlu <mikiasgd@gmail.com>
2022-09-19 18:45:59 +0300

    0-reset_to_98.c file commited

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - check if char is upper alphabet
+ *@x: char parameter
+ * _isupper: checks if it is upper case letter
+ * Return: zero or one
+ */
+
+void reset_to_98(int *n)
+{
+	return (*n = 98);
+}
