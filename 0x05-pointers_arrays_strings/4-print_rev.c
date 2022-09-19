1a4707d983ba47ae0a8362d0d6d34272a66d7356
mikias gedlu <mikiasgd@gmail.com>
2022-09-20 01:28:12 +0300

    hot fix 8

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,15 @@
 
 void print_rev(char *str)
 {
-	int size =0;
+	int size = 0;
 
 	while (*(str + size) != '\0')
 	{
 	size++;
 	}
-	while (0 <= size)
+	while (0 <= size - 1)
 	{
-	_putchar(*(str + size-1));
+	_putchar(*(str + size - 1));
 	size--;
 	}
 	_putchar('\n');
