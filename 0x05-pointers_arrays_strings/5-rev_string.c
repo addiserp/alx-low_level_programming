5cb88af0d716ad7b59d1cf5e92dc48a5f4f7042c
mikias gedlu <mikiasgd@gmail.com>
2022-09-20 01:35:02 +0300

    5-rev_string.c added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - prints the a string in reverse
+ *@str: char parameter
+ * Return: returns nothing
+ */
+
+void rev_string(char *str)
+{
+	int size = 0, s = 0;
+	char *temp
+	while (*(str + size) != '\0')
+	{
+	size++;
+	}
+	while (0 <= size - 1)
+	{
+	*(temp + s) = *(str + size - 1);
+	size--;
+	s++;
+	}
+	 *str = *temp;
+}
