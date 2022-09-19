dd3ca9591ee7970981a5904f7656ba96f514f0cb
mikias gedlu <mikiasgd@gmail.com>
2022-09-19 19:24:55 +0300

    3-puts.c function added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints the a string
+ *@s: char parameter
+ * Return: returns nothing
+ */
+
+void _puts(char *str)
+{
+	int s = 0;
+
+	while (*(str + s) != '\0')
+	stdout("%c", str + s)
+
+}
