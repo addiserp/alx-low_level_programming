eb9a10d4de848d4f03505b7f5b7a4acfac0626af
mikias gedlu <mikiasgd@gmail.com>
2022-09-20 19:28:53 +0300

    major update for 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,10 +20,10 @@ int _atoi(char *s)
 
 	for (i = 0; i < size; i++)
 	{
-		if (*(s + i) == '-')
+		if (*(s + i) == 45)
 			sign = 1;
-		else if (*(s + i) >= 0 && *(s + i) <= 9)
-			val *= 10 + *(s + i);
+		else if (*(s + i) >= 48 && *(s + i) <= 57)
+			val *= 10 + (*(s + i) - 48);
 	}
 		if (sign == 1)
 			val *= -1;
