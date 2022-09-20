e1bc5032c7f5fdae11e701e7b8f66a295639087c
mikias gedlu <mikiasgd@gmail.com>
2022-09-20 18:36:02 +0300

    hot fix for bounes 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,9 +17,9 @@ int _atoi(char *s)
 	size++;
 	}
 	for (i = 0;i<size; i++ )
-		if (*s + i) == '-')
+		if ((*s + i) == '-')
 			sign = 1;
-		else if (*s + i) >= 0 && (*s + i) <= 9)
+		else if ((*s + i) >= 0 && (*s + i) <= 9)
 			val*= 10 + (*s + i);
 		if (sign == 1)
 			val*=-1;
