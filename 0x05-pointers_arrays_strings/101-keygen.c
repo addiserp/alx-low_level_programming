15c89fe28e0f441ea89b1b5da1dbe765b5b75540
mikias gedlu <mikiasgd@gmail.com>
2022-09-20 19:49:39 +0300

    101-keygen.c added

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random Passwords
+ *return: return is zero
+ */
+int main(void)
+{
+	int pwd[100];
+	int i, n;
+	int val = 0;	
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pwd[i] = rand() % 48;
+		val += (pwd[i] + '0');
+		putchar(pwd[i] + '0');
+		if ((1551 - val) - '0' < 48)
+		{
+			n = 1551 - val - '0';
+			val += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
