a67c0c14352244ca118ba132e65e383f5126d756
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 18:28:53 +0300

    hot fix for 6-cap_string.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,12 +2,12 @@
 #define MAIN_H
 
 int _putchar(char c);                                                                                                                   
-char *_strcat(char *dest, char *src);                                                                                                   
-char *_strncat(char *dest, char *src, int n);                                                                                           
-char *_strncpy(char *dest, char *src, int n);                                                                                           
-int _strcmp(char *s1, char *s2);                                                                                                        
-void reverse_array(int *a, int n);                                                                                                      
+char *_strcat(char *, char *);                                                                                                   
+char *_strncat(char *, char *, int);                                                                                           
+char *_strncpy(char *, char *, int);                                                                                           
+int _strcmp(char *, char *);                                                                                                        
+void reverse_array(int *, int *);                                                                                                      
 char *string_toupper(char *);                                                                                                           
-char *cap_string(char *);
+char *cap_string(char *); 
 
 #endif
