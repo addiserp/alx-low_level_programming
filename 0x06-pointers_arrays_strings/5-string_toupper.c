f7bc376922fb9e4c0c0da80e03022f8267f16460
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 05:45:08 +0300

    5-string_toupper.c commited and added

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - function for alphabet to upper changes
+ * Description: check the alphabet is or not
+ *@x: is used for input parameter
+ * Return:0
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+	while (*(str + i) != '\0')
+	{
+	if (x >= 'A' && x <= 'z' && !(x > 92 && x < 97))
+		*(str + i) += 32;
+ 	}
+	retutn (str);
+}
