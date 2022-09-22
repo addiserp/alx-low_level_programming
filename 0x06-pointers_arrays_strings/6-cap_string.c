e4559f4aff3495625129960bb04b35d869e5eab4
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 19:06:15 +0300

    hot fix 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,19 +4,20 @@
  * Description: from a string 1st word
  * Return:0
  */
-
-char *cap_string(char *str)                                                                                                           
+char *cap_string(char *x)
 {
-int i = 0;
-while (str[i])
-{
-while (!(str[i] >= 'a' && str[i] <= 'z'))
-i++;
-if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || 
-	str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || 
-	str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0)                                                                                                       
-	str[i] -= 32;
+	int i = 0;
+	while (*(x + i) != '\0')
+	{
+	if (*(x + i) == '\n' || (*(x + i) >= 32 && *(x + i) <= 47 || *(x + i) >= 58 && *(x + i) <= 64))
+	{
+		i++;
+        if (*(x + i) == '\n' || (*(x + i) >= 32 && *(x + i) <= 47 || *(x + i) >= 58 && *(x + i) <= 64))
+        i++;
+        if ((*(x + i) >= 'a' && *(x + i) <= 'z'))
+        *(x + i) -= 32;
+	}
 	i++;
-	} 
-	return (str);
+    }
+	return (x);
 }
