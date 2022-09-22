618853c85dc60ef82b41a46511a17aac28221d4d
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 18:43:49 +0300

    hot fix 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,25 @@
 #include "main.h"
+
 /**
  * leet - function for alphabet encoding
- * Description: check the alphabet is or not
+ * Description: check the alphabet and encoding
  *@x: is used for input parameter
- * Return:0
+ * Return: value
  */
-char *leet(char * l)
+char *leet(char * x)
 {
 	int i = 0;
 	while (*(x + i) != '\0')
 	{
-	if (*(x + i) >= 'a' && *(x + i) <= 'A')
+	if (*(x + i) == 'a' || *(x + i) == 'A')
         *(x + i) = '4';
-	else if (*(x + i) >= 'e' && *(x + i) <= 'E')
+	else if (*(x + i) == 'e' || *(x + i) == 'E')
         *(x + i) = '3';
-	else if (*(x + i) >= 'o' && *(x + i) <= 'O')
+	else if (*(x + i) == 'o' || *(x + i) == 'O')
         *(x + i) = '0';
-	else if (*(x + i) >= 't' && *(x + i) <= 'T')
+	else if (*(x + i) == 't' || *(x + i) == 'T')
         *(x + i) = '7';
-	else if (*(x + i) >= 'l' && *(x + i) <= 'L')
+	else if (*(x + i) == 'l' || *(x + i) == 'L')
         *(x + i) = '1';
         i++;
     }
