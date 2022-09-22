9708a125d683f359a514680c0fefd215751af233
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 16:31:24 +0300

    HOT FIX 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
 * _strcat - concatnets two strings
 * @dest: destination
@@ -7,32 +7,13 @@
 * Return: destination
 */
 
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)                                                                                            
 {
-int i = 0, j = 0;
-int size1 = 0, size2 = 0, totsize = 0;
+int index = 0, dest_len = 0;
 
-while (*(dest + size1) != '\0')
-{
-size1++;
-}
-while (*(src + size2) != '\0')
-{
-size2++;
-}
-totsize = size1 + size2;
-char *temp = (char *) malloc(totsize);
-while (*(dest + i) != '\0')
-{
-*(temp + i) = *(dest + i);
-i++;
-}
-while (*(src + j) != '\0')
-{
-*(temp + i) = *(src + j);
-j++;
-i++;
-}
-*(temp + i) = '\0';
-return (temp);
+while (dest[index++])
+	dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);                                                                                                               
 }
