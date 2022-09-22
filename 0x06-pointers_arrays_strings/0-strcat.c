03eeba30f33b94f5bf5ff11017f950d3322ec083
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 16:37:37 +0300

    hot fix 1-strncat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * _strcat - concatnets two strings
 * @dest: destination
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,40 +1,23 @@
-#include "main.h"
-#include <stdlib.h>
-/**
-* _strncat - concatnets two strings
-* @dest: destination
-* @src: source
-* Return: destination
-*/
-
-char *_strncat(char *dest, char *src, int n)
-{
-int i = 0, j = 0;
-int size1 = 0, size2 = 0, totsize = 0;
-
-while (*(dest + size1) != '\0')
-{
-size1++;
-}
-while (*(src + size2) != '\0')
-{
-size2++;
-}
-totsize = size1 + size2;
-char *temp = (char *) malloc(totsize);
-while (*(dest + i) != '\0')
-{
-*(temp + i) = *(dest + i);
-i++;
-}
-while (*(src + j) != '\0')
-{
-*(temp + i) = *(src + j);
-if (j > n)
-break;
-j++;
-i++;
-}
-*(temp + i) = '\0';
-return (temp);
+#include "main.h"                                                                                                                    
+                                                                                                                                     
+/**                                                                                                                                  
+ * _strncat - Concatenates two strings using at most                                                                                 
+ *            an inputted number of bytes from src.                                                                                  
+ * @dest: The string to be appended upon.                                                                                            
+ * @src: The string to be appended to dest.                                                                                          
+ * @n: The number of bytes from src to be appended to dest.                                                                          
+ *                                                                                                                                   
+ * Return: A pointer to the resulting string dest.                                                                                   
+ */                                                                                                                                  
+char *_strncat(char *dest, char *src, int n)                                                                                         
+{                                                                                                                                    
+        int index = 0, dest_len = 0;                                                                                                 
+                                                                                                                                     
+        while (dest[index++])                                                                                                        
+                dest_len++;                                                                                                          
+                                                                                                                                     
+        for (index = 0; src[index] && index < n; index++)                                                                            
+                dest[dest_len++] = src[index];                                                                                       
+                                                                                                                                     
+        return (dest);                                                                                                               
 }
