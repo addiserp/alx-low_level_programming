1a536de6c480708a25673f650c3129970d9b4dc6
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 18:19:04 +0300

    6-cap_string.c commited

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,18 +4,20 @@
  * Description: from a string 1st word
  * Return:0
  */
-char *cap_string(char *)
+char *cap_string(char *x)
 {
 	int i = 0;
 	while (*(x + i) != '\0')
 	{
-	if ( (*(x + i) == 32 )
+	if (*(x + i) == '\n' || (*(x + i) >= 32 && *(x + i) <= 47 || *(x + i) >= 58 && *(x + i) <= 64))
 	{
-	i++;
-	if ( (*(x + i) >= 'a' && *(x + i) <= 'z'))
+    i++;
+        if (*(x + i) == '\n' || (*(x + i) >= 32 && *(x + i) <= 47 || *(x + i) >= 58 && *(x + i) <= 64))
+        i++;
+        if ((*(x + i) >= 'a' && *(x + i) <= 'z'))
         *(x + i) -= 32;
 	}
-        i++;
+	i++;
     }
 	return (x);
 }
