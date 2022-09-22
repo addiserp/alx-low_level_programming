57b9ea561cdf71e0c5fdce539401fb2d535c31d4
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 16:43:44 +0300

    hot fix 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,16 +5,14 @@
 * @dest: destination
 * @src: source
 * Return: destination
-*/                                                                                                                                  
+*/
 char *_strncat(char *dest, char *src, int n)                                                                                         
-{                                                                                                                                    
-        int i = 0, len = 0;                                                                                                 
-                                                                                                                                     
-        while (dest[i++])                                                                                                        
-                len++;                                                                                                          
-                                                                                                                                     
-        for (i = 0; src[i] && i < n; i++)                                                                            
-                dest[len++] = src[i];                                                                                       
-                                                                                                                                     
-        return (dest);                                                                                                               
+{
+	int i = 0, len = 0;
+
+		while (dest[i++])
+		len++;
+		for (i = 0; src[i] && i < n; i++)
+		dest[len++] = src[i];			
+		return (dest);                                                                                                               
 }
