c2463d0981e8e05b8fdcfffc59934a5e09ef6cd8
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 16:52:50 +0300

    Hot Fix 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,14 @@ int _strcmp(char *s1, char *s2)
 	int result = 0;
 	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
 	{
-		if (*(s1 + i) == *(s2 + i);
-		i++;
-		else if (*(s1 + i) > *(s2 + i);
+		if (*(s1 + i) == *(s2 + i))
+			i++;
+		else if (*(s1 + i) > *(s2 + i))
 		{
 			result = 1;
 			break;
 		}
-		else if (*(s1 + i) < *(s2 + i);
+		else if (*(s1 + i) < *(s2 + i))
 		{
 			result = -1;
 			break;
