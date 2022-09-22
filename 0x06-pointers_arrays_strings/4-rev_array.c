5c132a6fd6b15e31bb3895dc577b649013ad9b18
mikias gedlu <mikiasgd@gmail.com>
2022-09-22 05:30:15 +0300

    4-rev_array.c commited and added

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - prints the a string in reverse
+ *@a: array ofintegers
+ *@n size of array
+ * Return: returns nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	while (i < n)
+	{
+	temp = a[i];
+	a[i] = a[n-1];
+	a[n-1] = temp;
+	n--;
+	i++;
+	}
+}
