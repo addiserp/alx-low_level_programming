c8995f308aee56fbe71f41edee047fa10db3ca2f
mikias gedlu <mikiasgd@gmail.com>
2022-09-23 11:37:19 +0300

    hot fic 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,9 @@ int i = 0, dest_len = 0;
 
 while (dest[i++])
 {
-dest_len++;
+len++;
 	for (i = 0; src[i]; i++)
-		dest[dest_len++] = src[i];
+		dest[len++] = src[i];
 }
 	return (dest);                                                                                                               
 }
