394d7d7abe64ebc344bb3b9cac33425b17e63c6d
mikias gedlu <mikiasgd@gmail.com>
2022-09-23 11:35:24 +0300

    hot fix 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,11 +8,13 @@
 
 char *strcat(char *dest, const char *src)                                                                                            
 {
-int index = 0, dest_len = 0;
+int i = 0, dest_len = 0;
 
-while (dest[index++])
-	dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+while (dest[i++])
+{
+dest_len++;
+	for (i = 0; src[i]; i++)
+		dest[dest_len++] = src[i];
+}
 	return (dest);                                                                                                               
 }
