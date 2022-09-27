6a77c51ef7d1a7db6b89bf2d77dc7c658848ea33
mikias gedlu <mikiasgd@gmail.com>
2022-09-27 18:29:03 +0300

    hot fix bonues

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* set_string - assign the value of a pointer to a char.                                                                             
- * @s: pointer string                                                                                                                
- * @to: to char.                                                                                                                  
- */                                                                                                                                
-void set_string(char **s, char *to)                                                                                                
-{                                                                                                                                  
-        *s = to;                                                                                                                   
+*set_string - assign the value of a pointer to a char.
+*@s: pointer string
+*@to: to char.
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
 }
