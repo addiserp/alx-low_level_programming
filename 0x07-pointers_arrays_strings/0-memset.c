81d9db34629d5bbae9737b6c2266abd177a02946
mikias gedlu <mikiasgd@gmail.com>
2022-09-27 17:56:00 +0300

    hot fix 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,19 @@
 
 /**
  * _memset - that fills memory with a constant byte
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
+ * @s: A pointer to the memory.
+ * @c: The buffer character to fill.
+ * @n: The size of bytes to be filled.
  * description _memset: a function that fills memory with a constant byte
- * Return: void.
+ * Return: char.
  */
 
-char *_memset(char *s, char c, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 long unsigned int i;
-char *mem = s, val = c;
+char *mem = s;
 
 for (i = 0; i < n; i++)
-	mem[i] = val;
+	mem[i] = b;
 return (mem);
 }
