53825c9d7ca226bd5392d844d35c6452009a6a26
mikias gedlu <mikiasgd@gmail.com>
2022-09-27 18:20:05 +0300

    hot fix 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strspn - Gets the length of a pre substring.
+*_strspn - Gets the length of a pre substring.
 * @s: The string to be searched.
 * @accept: The prefix to be checked.
 * Return: The number of bytes in s
@@ -9,22 +9,21 @@
 
 unsigned int _strspn(char *s, char *accept)                                                                                          
 {
-	unsigned int bytes = 0;
+	unsigned int b = 0;
 	int i;
 	while (*s)
-
 	{
 	for (i = 0; accept[i]; i++)
 	{
 	if (*s == accept[i])
 	{
-	bytes++;
+	b++;
 	break;
 	}
 	else if (accept[i + 1] == '\0')
-		return (bytes);
-		}
-		s++;
-		}
-		return (bytes);
+		return (b);
+	}
+	s++;
+	}
+return (b);
 }
