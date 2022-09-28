70f38ac528ea98251c85db0793a5463819870cf2
mikias gedlu <mikiasgd@gmail.com>
2022-09-28 19:42:38 +0300

    hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
+#include <unistd.h>
 /**
  *_puts_recursion - Prints strings 
  *@s: A pointer to the memory.
  *description _puts_recursion: a function that prints single chars
  * Return: nothing.
  */
-void _puts_recursion(char *s)
+const void _puts_recursion(char *s)
 {
 if (*(s) != '\0')
 {
