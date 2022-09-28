a603ee9ecd74f24d542c6ba133367cb8dc942567
mikias gedlu <mikiasgd@gmail.com>
2022-09-28 22:37:18 +0300

    _strlen_recursion commited and added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - returns size of string
+ *@s: A pointer to the memory.
+ *description _strlen_recursion: a function that return size of string
+ * Return: sizes.
+ */
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+_strlen_recursion(s + 1);
+return size++;
+}
+}
