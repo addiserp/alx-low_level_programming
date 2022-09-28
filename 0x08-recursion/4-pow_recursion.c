3a67addb6473c5743d54e30aa8ecce468f49a1e0
mikias gedlu <mikiasgd@gmail.com>
2022-09-28 23:03:00 +0300

    hot fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,15 +3,14 @@
 /**
  *_pow_recursion - returns power of x and y
  *@x: number no.
- *@y: is a power 
+ *@y: is a power
  *description _pow_recursion: returns x the power of y
  * Return: value.
  */
 int _pow_recursion(int x, int y)
 {
- int value = x;
- 
- if (y < 0)
+int value = x;
+if (y < 0)
 	return (-1);
  else if (y == 0)
 	return (1);
