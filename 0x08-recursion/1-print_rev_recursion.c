b477a3c5341fee618e61c07451b71dd044c80e5c
mikias gedlu <mikiasgd@gmail.com>
2022-09-28 22:31:19 +0300

    update 1-print_rev_rec

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,44 +8,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i, size = 0;
-	char temp;
-
-	   _size(*s,&size);
-
-		for (i = 0; *(s + i) != '\0'; i++)
-		{
-			temp = s[i];
-			s[i] =  s[size -1];
-			s[size - 1] = temp;
-			size--;
-		}
-_puts_recursion1(s);
-}
-
-void _puts_recursion1(char *s)
+if (*s)
 {
-if (*(s) != '\0')
-{
-_putchar(*(s));
-if (*(s + 1) == '\0')
-{
-_putchar('\n');
-return;
-}
-_puts_recursion1(s += 1);
-}
-if (*(s) == '\0')
-{
-_putchar('\n');
-}
-}
-
-void _size(char *s,int *size)
-{
-if (*(s) != '\0')
-{
-*size += 1;
-_size(*s, &size);
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,7 +4,5 @@
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
-void _puts_recursion1(char *s);
 
-void _size(char *s,int *size)
 #endif
