5bea8811eb1e212472a6419c5e1bbe2dd71635c8
mikias gedlu <mikiasgd@gmail.com>
2022-09-28 22:11:09 +0300

    _puts_recursion1 added

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,18 +8,37 @@
  */
 void _print_rev_recursion(char *s)
 {
-int i, size = 0;
-char temp;
-for (i = 0; *(s + i) != '\0'; i++)
-	size ++;
-for (i = 0; *(s + i) != '\0'; i++)
-{
-temp = s[i];
-s[i] =  s[size -1];
-s[size - 1] = temp;
-size--;
-}
-_puts_recursion(s);
+	int i, size = 0;
+	char temp;
+
+		for (i = 0; *(s + i) != '\0'; i++)
+			size ++;
+
+		for (i = 0; *(s + i) != '\0'; i++)
+		{
+			temp = s[i];
+			s[i] =  s[size -1];
+			s[size - 1] = temp;
+			size--;
+		}
+_puts_recursion1(s);
 }
 
+void _puts_recursion1(char *s)
+{
+if (*(s) != '\0')
+{
+_putchar(*(s));
+if (*(s + 1) == '\0')
+{
+_putchar('\n');
+return;
+}
+_puts_recursion1(s += 1);
+}
+if (*(s) == '\0')
+{
+_putchar('\n');
+}
+}
 
