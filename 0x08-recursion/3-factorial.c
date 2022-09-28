f7c88d0e44aca3f2a058b5b29e414b1f4d033017
mikias gedlu <mikiasgd@gmail.com>
2022-09-28 22:49:53 +0300

    factorial added and commited

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *factorial - returns factorial of given number
+ *@n: given no.
+ *description factorial: returns factorial of given number
+ * Return: value.
+ */
+int factorial(int n)
+{
+while (n)
+n = n * factorial(n - 1);
+}
+return (n);
+}
