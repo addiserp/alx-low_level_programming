5644b293441022e12849a27f2266dbd0cce400da
mikias gedlu <mikiasgd@gmail.com>
2022-09-28 19:40:33 +0300

    _puts_recursion commited and added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - Prints strings 
+ *@s: A pointer to the memory.
+ *description _puts_recursion: a function that prints single chars
+ * Return: nothing.
+ */
+void _puts_recursion(char *s)
+{
+if (*(s) != '\0')
+{
+write(1, *(&s), 1);
+_puts_recursion(s+=1);
+}
+}
