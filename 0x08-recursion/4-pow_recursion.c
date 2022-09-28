7464193cb9fd9e0a2d393976b18d64a044fba8d6
mikias gedlu <mikiasgd@gmail.com>
2022-09-28 23:04:31 +0300

    4-pow_recursion.c hot fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,8 +12,8 @@ int _pow_recursion(int x, int y)
 int value = x;
 if (y < 0)
 	return (-1);
- else if (y == 0)
+else if (y == 0)
 	return (1);
- value *= _pow_recursion(x, y - 1);
+value *= _pow_recursion(x, y - 1);
 	return (value);
- }
+}
