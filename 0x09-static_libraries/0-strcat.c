555c007338688b69c51ee43d718910f80a17d21b
Mikias Gedlu <addiserp@gmail.com>
2022-09-30 09:25:55 -0700

    static lib

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -8,30 +8,5 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i = 0, j = 0;
-int size1 = 0, size2 = 0, totsize = 0;
-
-while (*(dest + size1) != '\0')
-{
-size1++;
-}
-while (*(src + size2) != '\0')
-{
-size2++;
-}
-totsize = size1 + size2;
-char *temp = (char *) malloc(totsize);
-while (*(dest + i) != '\0')
-{
-*(temp + i) = *(dest + i);
-i++;
-}
-while (*(src + j) != '\0')
-{
-*(temp + i) = *(src + j);
-j++;
-i++;
-}
-*(temp + i) = '\0';
-return (temp);
+return (dest = src);
 }
