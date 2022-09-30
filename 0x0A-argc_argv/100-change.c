10aff49eb1e3edd9709a786882331ffcb2adb507
mikias gedlu <mikiasgd@gmail.com>
2022-09-30 18:50:49 +0300

    hot fix 6

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,37 +2,38 @@
 #include <stdlib.h>
 #include <ctype.h>
 /**
- *main -  prints the minimum number of coins 
+ *main -  prints the minimum number of coins
  *@argc: value for no of arguments.
  *@argv: value for vector arguments.
- *Description: function  prints changes of coins 
+ *Description: function  prints changes of coins
  *Return: is always 0 if result is good
  */
 
 int main(int argc, char *argv[])
 {
 	int value = 0, count = 0;
+
 	if (argc == 2)
+	{
+		value = atoi(argv[1]);
+		if (value < 0)
+			{
+			printf("%d\n", 0);
+			return (0);
+			}
+		else if (isdigit(*argv[1]))
 		{
-				value = atoi(argv[1]);
-				if (value < 0)
-					{
-					printf("%d\n", 0);
-					return (0);
-					}
-				else if (isdigit(*argv[1]))
+			while (value >= 0)
+			{
+				if (value - 25 >= 0)
 				{
-					while (value >= 0)
-					{
-						if (value - 25 >= 0)
-						{
-							count += 1;
-							value = value - 25;
-						}
-					}
+					count += 1;
+					value = value - 25;
 				}
-		printf("%d\n", count);
-		return (0);
+			}
+		}
+	printf("%d\n", count);
+	return (0);
 		}
 	else
 	{
