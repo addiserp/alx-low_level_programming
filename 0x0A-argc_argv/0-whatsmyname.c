53f1becde621fd38bb2de9fc5c58b2e072b0d719
mikias gedlu <mikiasgd@gmail.com>
2022-09-30 16:20:23 +0300

    hot fix 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints its own name
- * Description: this is the main function program to display its own name
- * Return: is always 0
+ *main - prints its own name
+ *Description: function program to display its own name
+ *Return: is always 0
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s", argv[argc - 1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
