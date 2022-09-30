090288324208339580846b8795748e0fc9e97f2c
mikias gedlu <mikiasgd@gmail.com>
2022-09-30 15:55:05 +0300

    prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * main - prints its own name
+ * Description: this is the main function program to display its own name
+ * Return: is always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf(*argv[0]);
+	return (0);
+}
