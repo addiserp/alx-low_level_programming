3b8f90ad101f3dfd1f39e9591298e7de72d6c06c
mikias gedlu <mikiasgd@gmail.com>
2022-10-03 18:55:04 +0300

    0-create_array.c commited and added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *create_array -  creates dynamic arrays
+ *@size: size of array to be created.
+ *@c: sources string array.
+ *Description: function  creates dynamic arrays
+ *Return: will return new created char array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+    char *str;
+	str = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		*(str + i) = *(c +i);
+	}
+	return (str);
+}
