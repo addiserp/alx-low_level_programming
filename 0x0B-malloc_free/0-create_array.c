9947180cdbab0059fbf4226f6175900459409f31
mikias gedlu <mikiasgd@gmail.com>
2022-10-03 20:18:05 +0300

    hot fix 15

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,13 +15,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *str = malloc(sizeof(char) * size);
 
-	if  (*(str) == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (NULL);
-	}
-
+	
 	for (i = 0; i < size; i++)
+	{
+		if  (str == NULL)
+		{
+			printf("failed to allocate memory\n");
+			return (NULL);
+		}
+	}
 		*(str + i) = c;
 	return (str);
 }
