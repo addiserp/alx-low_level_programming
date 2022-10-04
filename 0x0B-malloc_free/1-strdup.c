523568b53eeb5b4b092a844f46dd7236ea98b4bc
mikias gedlu <mikiasgd@gmail.com>
2022-10-04 10:25:51 +0300

    hot fix 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,12 +11,17 @@ char *_strdup(char *str)
 {
 	int i = 0, size = 0;
 	char *dest;
+
 	while (*(str + size) != '\0')
 	{
 		size++;
 	}
-	dest = malloc(sizeof(char) * size);
-	
+	dest = malloc(sizeof(char) * size - 1);
+	if (dest == NULL)
+		{
+			printf("failed to allocate memory\n");
+			return (NULL);
+		}
 	while (*(str + i) != '\0')
 	{
 		*(dest + i) = *(str + i);
