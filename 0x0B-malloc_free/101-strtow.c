ff574c757b2e650a53381ac693fac13138033e02
mikias gedlu <mikiasgd@gmail.com>
2022-10-04 17:57:56 +0300

    101-strtow.c commited and added

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* argstostr - concatnets give parameters strings
+* @ac: size of parameter
+* @av: array vector
+* Return: newly creted dynamic array
+*/
+
+char **strtow(char *str)
+{
+	char **s;
+	int i, len = 0, len2 = 0;
+	
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	len = len(str);
+	if (len == 0)
+		return (NULL);
+	s = malloc(sizeof(char *) * (len + 1));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		{
+			len2 = stringwordcount;
+			s[i] = malloc(sizeof(char) * (len2 + 1));
+				if (s[i] == NULL)
+					{
+					for (;i >= 0; i--)
+					free(s[i]);
+					free(s);
+					return (NULL);
+					}
+			for (j = 0;j < len2; j++)
+				s[i][j] = str[len++];
+			s[i][j] = '\0';
+		}
+		s[i] = NULL;
+		return (s);
+}
