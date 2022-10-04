949a50cf710249f0e8cf79a9ba602eee066dcfcb
mikias gedlu <mikiasgd@gmail.com>
2022-10-04 18:24:23 +0300

    hot fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,22 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* argstostr - concatnets give parameters strings
+* strtow - splits a string into words
 * @s: to be countd
- *Description strtow: in a string.
- *Description stringwordcount:  counts words in a string.
+ *Description strtow : in a string.
+ *Description stringwordcount :  counts words in a string.
 * Return: newly creted dynamic array
 */
 int stringwordcount(char *s)
 {
-	int i,words=0;
+	int i, words = 0;
 
-	for(i=0;s[i];i++)
+	for(i = 0; s[i]; i++)
 	{
-	if(s[i]==32)
+	if (s[i] == 32)
 	words++;
  	}
-	if(i>0)
+	if (i > 0)
 	words++;
 	return words;
 }
@@ -26,7 +26,7 @@ char **strtow(char *str)
 {
 	char **s;
 	int i, j, len = 0, len2 = 0;
-	
+
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
 	len = strlen(str);
@@ -36,20 +36,20 @@ char **strtow(char *str)
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
-		{
-			len2 = stringwordcount(*s);
-			s[i] = malloc(sizeof(char) * (len2 + 1));
-				if (s[i] == NULL)
-					{
-					for (;i >= 0; i--)
-					free(s[i]);
-					free(s);
-					return (NULL);
-					}
-			for (j = 0;j < len2; j++)
-				s[i][j] = str[len++];
-			s[i][j] = '\0';
-		}
+	{
+		len2 = stringwordcount(*s);
+		s[i] = malloc(sizeof(char) * (len2 + 1));
+			if (s[i] == NULL)
+				{
+				for (; i >= 0; i--)
+				free(s[i]);
+				free(s);
+				return (NULL);
+				}
+		for (j = 0; j < len2; j++)
+			s[i][j] = str[len++];
+		s[i][j] = '\0';
+	}
 		s[i] = NULL;
 		return (s);
 }
