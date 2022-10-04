eb583d4c9ccc812f59b73a457bb2e6724722760d
mikias gedlu <mikiasgd@gmail.com>
2022-10-04 18:11:10 +0300

    hot fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,7 +8,20 @@
 * @av: array vector
 * Return: newly creted dynamic array
 */
-
+int stringwordcount(char *s)
+{
+    int i,words=0;
+	for(i=0;s[i];i++)  
+    {
+    	if(s[i]==32)
+    	 words++;
+ 
+ 	}
+ 	if(i>0)
+      words++;
+	  
+	return words; 	
+}
 char **strtow(char *str)
 {
 	char **s;
