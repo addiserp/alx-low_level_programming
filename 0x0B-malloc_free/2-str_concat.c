4dbc2c213191c0c8903c0ba1a2eecd9f39ab09c5
mikias gedlu <mikiasgd@gmail.com>
2022-10-04 11:06:53 +0300

    hot fix 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,7 +8,7 @@
 * @src: source
 * Return: destination
 */
-char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2)
 {
 		int i = 0, j = 0, len1 = 0, len2 = 0, totlen = 0;
 		char *str;
