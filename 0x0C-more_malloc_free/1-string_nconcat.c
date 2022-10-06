e511c710079116786d97cf33666295c7d4293ee9
mikias gedlu <mikiasgd@gmail.com>
2022-10-06 23:04:33 +0300

    1-string_ncontact.c added and commited

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,5 +14,26 @@
 
  char *string_nconcat(char *s1, char *s2, unsigned int n)
  {
- 
- }
+	char *p;
+	unsigned int i, x =0; y = 0;
+	
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
+	while (s1[x] != '\0')
+	x++;
+	while (s2[y] != '\0')
+	y++;
+	if (n > y)
+	n = y;
+	p = malloc((x + n + 1) * sizeof(char));
+	if (p == NULL)
+	return (0);
+	for (i= 0; i < x; i++)
+	p[i] = s1[i];
+	for ( ; i < x + n; i++)
+	p[i] = s2[i - x];
+	p[i] = '0\';
+return (p);
+}
