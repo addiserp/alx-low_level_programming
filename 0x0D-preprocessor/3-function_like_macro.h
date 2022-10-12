66bdca5334699660cc1d68e400c59e39d6f69ce2
mikias gedlu <mikiasgd@gmail.com>
2022-10-12 19:05:22 +0300

    hot fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
 /**
  *ABS - absolut value of given no
+ *@x: parameter 1.
  *Description - ABS: is macro token.
  *Return: Always value.
  */
 
-#define ABS(x) ( (x) < (0) ? ((x) * (-1)) : (x))
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+#endif
