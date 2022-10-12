874ff160607dd64cd6a7ebd7a664d0be7073413e
mikias gedlu <mikiasgd@gmail.com>
2022-10-12 19:00:41 +0300

    hot fix

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,7 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
 /**
  * SUM - ADDS value of given no.
+ *@x: parameter 1.
+ *@y: parameter 2.
  *Description - SUM: added two numbers.
  * Return: Always value.
  */
 
 #define SUM(x, y) ((x) + (y))
+
+#endif
