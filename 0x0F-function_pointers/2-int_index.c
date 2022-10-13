4bcd5938adb28c741b2f7bdd578d03ab26118587
mikias gedlu <mikiasgd@gmail.com>
2022-10-13 03:55:48 +0300

    2-int_index.c added and commited

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pinters.h"
+#include <stdlib.h>
+
+/**
+ *int_index - a function that searches for an integer.
+ *@array: paramenter string.
+ *@size: pointer size.
+ *@cmp: function pointer
+ *Description - int_index: a function that searches for an integer.
+ *Return: integer type.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array && cmp)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (cmp(array[x] != 0))
+			return;
+		}
+	}
+}
