e703cec583c7b5425a0ed09f062c98c24e21ca67
mikias gedlu <addiserp@gmail.com>
2022-10-20 20:18:00 +0300

    0-print list commited and added

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ *print_list - a function that PRINTS linked list.
+ *@h: The type of parameters.
+ *Description - print_list: a function that prints all its parameters.
+ *Return: - returns node type size_t.
+ */
+
+size_t print_list(const list_t *h)
+{
+size_t n = 0;
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)");
+else
+printf("[%u] %s\n", h->len, h->str);
+n++;
+h = h->next;
+}
+
+return (n);
+}
