f275f19f0588837b0acb7aa9cc65f5fe831db9b2
mikias gedlu <mikiasgd@gmail.com>
2022-10-28 04:30:45 +0300

    hot fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,11 +8,11 @@
  */
 unsigned int binary_to_uint(const char* b)
 {
-	unsigned int result = 0, i = 0;
-
+	unsigned int result = 0;
+	int i;
 	if (b == NULL)
 		return (0);
-	for (; b[i]; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i]) < '0' || b[i] > '1')
 		return (0);
