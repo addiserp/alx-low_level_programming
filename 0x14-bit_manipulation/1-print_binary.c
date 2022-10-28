5183f616d7cb6e28de92086308645bc55f4c70d3
mikias gedlu <mikiasgd@gmail.com>
2022-10-28 04:43:44 +0300

    1-print_binary.c commited and added

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_binary -  print binary values.
+ *@b: binary parameter.
+ *Description - print_binary: print binary to base 10.
+ *Return: void.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+void _putchar(char);
 unsigned int binary_to_uint(const char* b);
+void print_binary(unsigned long int n);
 
 #endif
 
