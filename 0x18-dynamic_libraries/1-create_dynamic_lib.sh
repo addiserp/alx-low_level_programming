3643174b65e9d0c019ba77590a594b37e36e90e0
mikias gedlu <addiserp@gmail.com>
2022-12-12 18:52:42 +0300

    0x18-dynamic_libraries commited and added and commited

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -2,3 +2,7 @@
 gcc -fPIC -c *.c
 gcc -shared -o libdynamic.so *.o
 nm -D libdynamic.so
+gcc -Wall -pedantic -Werror -Wextra -L. 0-main.c -ldynamic -o len
+ldd len
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+./len
