dba3898c9891aabbb52a6870cc2546854534453e
mikias gedlu <addiserp@gmail.com>
2022-12-12 19:14:10 +0300

    100-operations.so commited and added

---
+++ 0x18-dynamic_libraries/calc.h
@@ -0,0 +1,12 @@
+#ifndef CALC_H
+#define CALC_H
+
+
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif
---
+++ 0x18-dynamic_libraries/pycalc.c
@@ -0,0 +1,57 @@
+#include "calc.h"
+
+ /**
+ *add - a function that add an integer.
+ *@a: paramenter 1.
+ *@b: parameter 2.
+ *Description - add: a function that add an integer.
+ *Return: value.
+ */
+ /**
+ *ub - a function that substruct an integer.
+ *@a: paramenter 1.
+ *@b: parameter 2.
+ *Description - sub: a function that substruct an integer.
+ *Return: value.
+ */
+ /**
+ *mul - a function that muliply an integer.
+ *@a: paramenter 1.
+ *@b: parameter 2.
+ *Description - mul: a function that muliply an integer.
+ *Return: value.
+ */
+  /**
+ *div - a function that divid an integer.
+ *@a: paramenter 1.
+ *@b: parameter 2.
+ *Description - div: a function that divid an integer.
+ *Return: value.
+ */
+  /**
+ *mod - a function that get moded an integer.
+ *@a: paramenter 1.
+ *@b: parameter 2.
+ *Description - mod: a function that get moded an integer.
+ *Return: value.
+ */
+ int add(int a, int b)
+{
+return (a + b);
+}
+int sub(int a, int b)
+{
+return (a - b);
+}
+int mul(int a, int b)
+{
+return (a * b);
+}
+int div(int a, int b)
+{
+return (a / b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
+}
