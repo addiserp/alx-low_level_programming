5a3e3d31563c3c7fdda3708f4afb5e2a8fa62224
Mikias Gedlu <addiserp@gmail.com>
2023-09-01 14:27:36 +0300

    0x17-doubly_linked_lists 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,22 +8,21 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    
-    dlistint_t *add_node;
 
-	add_node = malloc(sizeof(dlistint_t));
+dlistint_t *add_node;
 
-	if (add_node == NULL)
-		return (NULL);
-    
-    add_node->n = n;
-    
-    add_node->next = *head;
-    
-    add_node->prev = NULL;
-	if ((*head) != NULL)
-		(*head)->prev = add_node;
-	(*head) = add_node;
+add_node = malloc(sizeof(dlistint_t));
 
-	return (add_node);
+if (add_node == NULL)
+    return (NULL);
+
+add_node->n = n;
+add_node->next = *head;
+add_node->prev = NULL;
+if ((*head) != NULL)
+    (*head)->prev = add_node;
+
+(*head) = add_node;
+
+return (add_node);
 }
