402e3e524b68d312faeadde86540ecf538e7c5e6
Mikias Gedlu <addiserp@gmail.com>
2023-09-09 18:44:52 +0300

    0x1A-hash_tables 4

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -85,6 +85,7 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
+char *hash_table_get(const hash_table_t *ht, const char *key);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
