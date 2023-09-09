25a23e44762660cdfe8c76f2d5ec1a800cf8ca2d
Mikias Gedlu <addiserp@gmail.com>
2023-09-09 18:51:07 +0300

    0x1A-hash_tables 6

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -85,6 +85,7 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
+void hash_table_delete(hash_table_t *ht);
 void hash_table_print(const hash_table_t *ht);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
